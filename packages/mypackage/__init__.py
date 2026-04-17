1045cf5351a5a6111c46f5713bc6563ccabb0abc
prakharInteger23 <prakhar@integertel.com>
2026-04-17 20:14:20 +0530

    .

---
+++ packages/mypackage/__init__.py
@@ -0,0 +1,4 @@
+from .math import add
+from .utils import utils_function
+
+__all__ = ['add', 'utils_function']
---
+++ packages/mypackage/utils.py
@@ -0,0 +1,4 @@
+def utils_function():
+    return "This is a utility function"
+
+print(utils_function())
